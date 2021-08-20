ecb046dc81b887dc3f739dbb5f92acdb3c723dc9
Abigail García <80718280+Aby3005@users.noreply.github.com>
2021-08-20 07:59:22 -0600

    Add files via upload

---
+++ pruebasreact/src/components/Form.jsx
@@ -0,0 +1,41 @@
+import React,{useState} from 'react';
+import Todo from '../components/Todo';
+
+const Form = () => {
+    const [todo, setTodo] = useState({})
+    const [todos, SetTodos] = useState([
+        {todo: 'todo 1'},
+        {todo: 'todo 2'},
+        {todo: 'todo 3'}
+    ])
+    const handleChange = e => setTodo({[e.target.name]: e.target.value})
+    const handleClick = e => {
+        if(Object.keys(todo).length === 0 || todo.todo.trim() === ''){
+            alert('el campo no puede estar vacío')
+            return
+        }
+        SetTodos([...todos, todo])
+    }
+
+    const deleteTodo = indice => {
+        const newTodos = [...todos]
+        newTodos.splice(indice,1)
+        SetTodos(newTodos)
+    }
+
+return (
+    <>
+    <form onSubmit={e=> e.preventDefault()}>
+        <label>Agregar tarea</label><br/>
+        <input type="text" name="todo" onChange={handleChange}/>
+        <button onClick={handleClick}>Agregar</button>
+    </form>
+    {
+        todos.map((value, index) => (
+        <Todo todo={value.todo} key={index} index={index} deleteTodo={deleteTodo}/>
+        ))
+    }
+</>
+)
+}
+export default Form
